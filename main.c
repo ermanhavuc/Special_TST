5544e5c0dcdbde0710c4e3ce0e625255003171eb
ermanhavuc <ermanhavuc@gmail.com>
2018-12-01 13:00:30 +0300

    Delete tree fixed and menu added

--- main.c
+++ main.c
@@ -4,141 +4,152 @@
 
 struct tst {
     int data;
-    struct bin_tree * right, * mid, * left;
+    struct bin_tree *right, *mid, *left;
 };
 typedef struct tst node;
 
-int inputarray[200];
-int max_size=0;
+int pInt[200];
+int max_size = 0;
 
-void insert(node ** tree, int val)
-{
+void insert(node **tree, int val) {
     node *temp = NULL;
-    if(!(*tree))
-    {
-        temp = (node *)malloc(sizeof(node));
+    if (!(*tree)) {
+        temp = (node *) malloc(sizeof(node));
         temp->left = temp->right = temp->mid = NULL;
         temp->data = val;
         *tree = temp;
         return;
     }
 
-    if(val < (*tree)->data)
-    {
+    if (val < (*tree)->data) {
         insert(&(*tree)->left, val);
-    }
-    else if(val > (*tree)->data && val < pow((*tree)->data,2))
-    {
+    } else if (val > (*tree)->data && val < pow((*tree)->data, 2)) {
         insert(&(*tree)->mid, val);
-    }
-    else if(val > pow((*tree)->data,2))
-    {
+    } else if (val > pow((*tree)->data, 2)) {
         insert(&(*tree)->right, val);
     }
-
 }
 
-node* find(node ** tree, int val)
-{
-    if(!(*tree))
-    {
+node *find(node **tree, int val) {
+    if (!(*tree)) {
         return NULL;
     }
 
-    if(val < (*tree)->data)
-    {
+    if (val < (*tree)->data) {
         find(&(*tree)->left, val);
-    }
-    else if(val > (*tree)->data && val < pow((*tree)->data,2))
-    {
+    } else if (val > (*tree)->data && val < pow((*tree)->data, 2)) {
         find(&(*tree)->mid, val);
-    }
-    else if(val > pow((*tree)->data,2))
-    {
+    } else if (val > pow((*tree)->data, 2)) {
         find(&(*tree)->right, val);
-    }
-    else if(val == (*tree)->data)
-    {
+    } else if (val == (*tree)->data) {
         return *tree;
     }
 }
 
-int print_preorder(node * tree)
-{
-    if (tree)
-    {
-        printf("%d\n",tree->data);
+int print_preorder(node *tree) {
+    if (tree) {
+        printf("%d\n", tree->data);
         print_preorder(tree->left);
         print_preorder(tree->mid);
         print_preorder(tree->right);
     }
 }
 
-size_t FindIndex( const int a[], size_t size, int value )
-{
+int FindIndex(const int a[], size_t size, int value) {
     size_t index = 0;
 
-    while ( index < size && a[index] != value ) ++index;
+    while (index < size && a[index] != value) ++index;
 
-    return ( index == size ? -1 : index );
+    return (index == size ? -1 : index);
 }
 
-node* delete(int val)
-{
-    node* root = NULL;
+node *delete(int val) {
+    node *root = NULL;
+    int position = FindIndex(pInt, max_size, val);
 
-    int position = FindIndex(inputarray, max_size, val);
+    if (position != -1) {
+        for (int c = position; c < max_size - 1; c++)
+            pInt[c] = pInt[c + 1];
 
-    for (int c = position; c < max_size - 1; c++)
-        inputarray[c] = inputarray[c+1];
+        max_size--;
 
-    max_size--;
-
-    for(int i = 0; i < max_size; i++){
-        insert(&root, inputarray[i]);
+        for (int i = 0; i < max_size - 1; i++) {
+            insert(&root, pInt[i]);
+        }
+    } else {
+        printf("Can't find this value in TST!\n");
     }
-
     return root;
-}
-
-void deltree(node * tree)
-{
-    if (tree)
-    {
-        deltree(tree->left);
-        deltree(tree->mid);
-        deltree(tree->right);
-        free(tree);
-    }
 }
 
 int main() {
     FILE *fp;
     fopen_s(&fp, "../input.txt", "r");
 
-    node *root;
+    node *root = NULL;
     node *tmp = NULL;
 
-    root = NULL;
+    int input = 0;
+    int i = 0;
+    int menu;
 
-    int input=0;
-    int i=0;
-    int a;
+    printf("Welcome to TST handler program!\n\n");
 
-    while(fscanf_s(fp, "%d", &input) != EOF){
+    while (fscanf_s(fp, "%d", &input) != EOF) {
         insert(&root, input);
-        inputarray[i]=input;
-        max_size++;
+        pInt[i] = input;
         i++;
     }
 
-    //printf("%d",print_preorder(root));
-    //tmp = find(&root, 75);
-    //node * deneme = removea(root, &tmp, 75);
+    max_size = i;
 
-    print_preorder(delete(75));
-    printf("\n");
-    print_preorder(delete(1700));
+    while (1) {
+        printf("Please choose:\n");
+        printf("1. Insert a value\n");
+        printf("2. Delete a value\n");
+        printf("3. Find a value\n");
+        printf("4. Print the TST\n");
+        printf("0. Exit\n");
+
+        scanf_s("%d", &menu);
+
+        switch (menu) {
+            case 1:
+                printf("Enter the value: \n");
+                scanf_s("%d", &menu);
+                insert(&root, menu);
+                max_size++;
+                pInt[max_size - 1] = menu;
+                break;
+            case 2:
+                printf("Enter the value: \n");
+                scanf_s("%d", &menu);
+                root = delete(menu);
+                break;
+            case 3:
+                printf("Enter the value: \n");
+                scanf_s("%d", &menu);
+                tmp = find(&root, menu);
+                if (tmp) {
+                    printf("Searched node=%d\n", tmp->data);
+                } else {
+                    printf("Data Not found in tree.\n");
+                }
+                break;
+            case 4:
+                printf("\n");
+                print_preorder(root);
+                printf("\n");
+                break;
+            case 0:
+                break;
+            default:
+                printf("Please enter a valid input\n");
+        }
+
+        if (menu == 0)
+            break;
+    }
 
     return 0;
 }
